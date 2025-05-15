f49c8aab180a2f57bf8b4cab0db50f07f4cb1b39
Luis Eduardo Sagnay Pilamunga <sagnayluis99@gmail.com>
2025-05-15 00:00:16 -0500

    edit conservation activities

--- forestal/src/main/java/edu/espe/cotbert/forestal/infraestructure/config/ConnectionDB.java
+++ forestal/src/main/java/edu/espe/cotbert/forestal/infraestructure/config/ConnectionDB.java
@@ -12,7 +12,7 @@ public class ConnectionDB {
 
     
 
-    private static final String URL = "jdbc:postgresql://localhost:5433/forestal";
+    private static final String URL = "jdbc:postgresql://localhost:5432/forestal";
     private static final String USER = "forestal_tree";
     private static final String PASSWORD = "admin123";
     private static final Logger logger = LoggerConfig.getLogger();

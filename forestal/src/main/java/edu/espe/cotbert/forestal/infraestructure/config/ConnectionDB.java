fb094337c3297dfb39266a50db7a796bd57b700c
Cotbert2 <mateojgdev@gmail.com>
2025-05-14 21:42:58 -0500

    change landing page

--- forestal/src/main/java/edu/espe/cotbert/forestal/infraestructure/config/ConnectionDB.java
+++ forestal/src/main/java/edu/espe/cotbert/forestal/infraestructure/config/ConnectionDB.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package edu.espe.cotbert.forestal.infraestructure.config;
 
 import edu.espe.cotbert.forestal.infraestructure.config.LoggerConfig;

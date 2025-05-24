3aea2a784ba2f82729f3eb112657ad92a7f63cb1
Cotbert2 <mateojgdev@gmail.com>
2025-05-24 12:53:30 -0500

    minor changes

--- client/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron');
-const soapService = require('./forestalservice.js');
-
-contextBridge.exposeInMainWorld('api', {
-  getForestalZones: soapService.getForestalZones,
-  getTreeSpecies: soapService.getTreeSpecies,
-  deleteTreeSpecies: soapService.deleteTreeSpecies,
-  createForestalZone: soapService.createForestalZone
-});

f6d45bb7321b7a72ad41fb1a1da35bfc01a2cd91
Cotbert2 <mateojgdev@gmail.com>
2025-05-24 12:57:33 -0500

    minor changes

--- client/index.ts
+++ client/index.ts
@@ -1,5 +1,5 @@
 // main.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow,Menu,dialog } = require('electron');
 const path = require('path');
 
 function createWindow() {
@@ -13,6 +13,33 @@ function createWindow() {
   });
 
   win.loadFile('./public/index.html');
+  const menuTemplate = [
+    {
+      label: 'App',
+      submenu: [
+        {
+          label: 'About developer',
+          click: () => {
+            dialog.showMessageBox(win, {
+              type: 'info',
+              title: 'Dev Info',
+              message: 'Developed by Team 3\nClient SOAP for Forestal JSO Project\nVersión 1.0',
+              buttons: ['OK']
+            });
+          }
+        },
+        {
+          label: 'Quit',
+          accelerator: 'CmdOrCtrl+Q',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
   // win.webContents.openDevTools();
 }
 
